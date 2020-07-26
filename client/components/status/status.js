9dab91c493d14eef93829a95d7b9758ff601c82a
Vivek Chand <V.rajput40@gmail.com>
2020-07-26 20:48:36 +0530

    [feature] perform captive portal login and logout

--- client/components/status/status.js
+++ client/components/status/status.js
@@ -1,55 +1,178 @@
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import axios from "axios";
 import PropTypes from "prop-types";
 import qs from "qs";
 import React from "react";
-import { Cookies } from "react-cookie";
-import { Link } from 'react-router-dom';
-import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
-import { validateApiUrl, loginError, logoutSuccess, mainToastId } from "../../constants";
+import {Cookies} from "react-cookie";
+import {Link} from "react-router-dom";
+import {toast} from "react-toastify";
+import {
+  genericError,
+  getUserRadiusSessionsUrl,
+  logoutSuccess,
+  mainToastId,
+  validateApiUrl,
+} from "../../constants";
 import getText from "../../utils/get-text";
 import logError from "../../utils/log-error";
 import Contact from "../contact-box";
 
 export default class Status extends React.Component {
-  componentDidMount() {
-    const { cookies, orgSlug, logout } = this.props;
+  constructor(props) {
+    super(props);
+    this.loginIfameRef = React.createRef();
+    this.loginFormRef = React.createRef();
+    this.logoutIfameRef = React.createRef();
+    this.logoutFormRef = React.createRef();
+    this.state = {
+      username: "",
+      password: "",
+      sessions: [],
+    };
+    this.validateToken = this.validateToken.bind(this);
+    this.getUserRadiusSessions = this.getUserRadiusSessions.bind(this);
+  }
+
+  async componentDidMount() {
+    const {cookies, orgSlug} = this.props;
+    // to prevent recursive call in case redirect url is status page
+    if (window.top === window.self) {
+      const {sessions} = this.state;
+      const isValid = await this.validateToken();
+      if (isValid) {
+        const macaddr = cookies.get(`${orgSlug}_macaddr`);
+        if (macaddr) {
+          await this.getUserRadiusSessions();
+          if (sessions && sessions.length === 0) {
+            this.loginFormRef.current.submit();
+          }
+        } else {
+          this.loginFormRef.current.submit();
+        }
+      }
+    }
+  }
+
+  async getUserRadiusSessions() {
+    const {cookies, orgSlug, logout} = this.props;
     const token = cookies.get(`${orgSlug}_auth_token`);
-    const url = validateApiUrl.replace("{orgSlug}", orgSlug);
-    return axios({
-      method: "post",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      url,
-      data: qs.stringify({
-        token,
-      }),
-    })
-      .then(response => {
-        if (response.data["control:Auth-Type"] !== "Accept") {
+    const url = getUserRadiusSessionsUrl(orgSlug);
+    const macaddr = cookies.get(`${orgSlug}_macaddr`);
+    try {
+      const response = await axios({
+        method: "post",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+        url,
+        data: qs.stringify({
+          token,
+          macaddr,
+        }),
+      });
+      this.setState({sessions: response.data});
+    } catch (error) {
+      logout(cookies, orgSlug);
+      toast.error(genericError, {
+        onOpen: () => toast.dismiss(mainToastId),
+      });
+      logError(error, genericError);
+    }
+  }
+
+  handleLogout = async () => {
+    const {orgSlug, logout, cookies} = this.props;
+    const {sessions} = this.state;
+    await this.getUserRadiusSessions();
+    if (sessions.length > 0) {
+      this.logoutFormRef.current.submit();
+      setTimeout(() => {}, 0);
+    }
+    logout(cookies, orgSlug);
+    toast.success(logoutSuccess);
+  };
+
+  handleLoginIframe = () => {
+    const {cookies, orgSlug, logout, captivePortalLoginForm} = this.props;
+
+    if (this.loginIfameRef && this.loginIfameRef.current) {
+      try {
+        const searchParams = new URLSearchParams(
+          this.loginIfameRef.current.contentWindow.location.search,
+        );
+        const reply = searchParams.get("reply");
+        const macaddr = searchParams.get(
+          captivePortalLoginForm.macaddr_param_name,
+        );
+        if (
+          reply &&
+          this.loginIfameRef.current.contentDocument.title.indexOf("404") < 0
+        ) {
           logout(cookies, orgSlug);
-          toast.error(loginError, {
+          toast.error(reply, {
             onOpen: () => toast.dismiss(mainToastId),
           });
-          logError(response, '"control:Auth-Type" !== "Accept"');
         }
-      })
-      .catch((error) => {
+        if (macaddr) {
+          cookies.set(`${orgSlug}_macaddr`, macaddr, {path: "/"});
+        }
+      } catch {
+        //
+      }
+    }
+  };
+
+  async validateToken() {
+    const {cookies, orgSlug, logout} = this.props;
+    const token = cookies.get(`${orgSlug}_auth_token`);
+    const url = validateApiUrl(orgSlug);
+    try {
+      const response = await axios({
+        method: "post",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+        url,
+        data: qs.stringify({
+          token,
+        }),
+      });
+      if (response.data.response_code !== "AUTH_TOKEN_VALIDATION_SUCCESSFUL") {
         logout(cookies, orgSlug);
-        toast.error(loginError, {
+        toast.error(genericError, {
           onOpen: () => toast.dismiss(mainToastId),
         });
-        logError(error, loginError);
+        logError(
+          response,
+          '"response_code" !== "AUTH_TOKEN_VALIDATION_SUCCESSFUL"',
+        );
+      } else {
+        const {radius_user_token: password, username} = response.data;
+        this.setState({username, password});
+      }
+      return true;
+    } catch (error) {
+      logout(cookies, orgSlug);
+      toast.error(genericError, {
+        onOpen: () => toast.dismiss(mainToastId),
       });
+      logError(error, genericError);
+      return false;
+    }
   }
 
   render() {
-    const { statusPage, language, orgSlug, logout, cookies } = this.props;
-    const { content, links, buttons } = statusPage;
+    const {
+      statusPage,
+      language,
+      orgSlug,
+      captivePortalLoginForm,
+      captivePortalLogoutForm,
+    } = this.props;
+    const {content, links, buttons} = statusPage;
+    const {username, password, sessions} = this.state;
     const contentArr = getText(content, language).split("\n");
     return (
       <>
@@ -67,14 +190,14 @@ export default class Status extends React.Component {
               })}
               {links
                 ? links.map(link => (
-                  <Link
-                    className="owisp-status-link"
-                    key={link.url}
-                    to={link.url.replace("{orgSlug}", orgSlug)}
-                  >
-                    {getText(link.text, language)}
-                  </Link>
-                ))
+                    <Link
+                      className="owisp-status-link"
+                      key={link.url}
+                      to={link.url.replace("{orgSlug}", orgSlug)}
+                    >
+                      {getText(link.text, language)}
+                    </Link>
+                  ))
                 : null}
               {buttons.logout ? (
                 <>
@@ -95,10 +218,7 @@ export default class Status extends React.Component {
                     className="owisp-status-btn owisp-status-logout-btn owisp-btn-primary "
                     id="owisp-status-logout-btn"
                     value={getText(buttons.logout.text, language)}
-                    onClick={() => {
-                      logout(cookies, orgSlug);
-                      toast.success(logoutSuccess);
-                    }}
+                    onClick={this.handleLogout}
                   />
                 </>
               ) : null}
@@ -108,6 +228,87 @@ export default class Status extends React.Component {
             </div>
           </div>
         </div>
+        {/* check to ensure this block of code is executed in root document and not in Iframe */}
+        {captivePortalLoginForm && window.top === window.self && (
+          <>
+            <form
+              ref={this.loginFormRef}
+              method={captivePortalLoginForm.method || "post"}
+              id="cp-form"
+              action={captivePortalLoginForm.action || ""}
+              target="owisp-auth-iframe"
+              className="owisp-auth-hidden"
+            >
+              <input
+                readOnly
+                type="text"
+                name={captivePortalLoginForm.fields.username || ""}
+                value={username}
+              />
+              <input
+                readOnly
+                type="text"
+                name={captivePortalLoginForm.fields.password || ""}
+                value={password}
+              />
+              {captivePortalLoginForm.additional_fields.length > 0
+                ? captivePortalLoginForm.additional_fields.map(field => (
+                    <input
+                      readOnly
+                      type="text"
+                      name={field.name}
+                      value={field.value}
+                    />
+                  ))
+                : null}
+            </form>
+            {/* login form is submitted in this Iframe
+            onLoad: handles response from captive portal
+            */}
+            <iframe
+              onLoad={this.handleLoginIframe}
+              ref={this.loginIfameRef}
+              name="owisp-auth-iframe"
+              className="owisp-auth-hidden"
+              title="owisp-auth-iframe"
+            />
+          </>
+        )}
+        {captivePortalLogoutForm && window.top === window.self && (
+          <>
+            <form
+              ref={this.logoutFormRef}
+              method={captivePortalLogoutForm.method || "post"}
+              id="cp-form"
+              action={captivePortalLogoutForm.action || ""}
+              target="owisp-auth-logout-iframe"
+              className="owisp-auth-hidden"
+            >
+              <input
+                readOnly
+                type="hidden"
+                name={captivePortalLogoutForm.fields.id || ""}
+                value={sessions.length > 0 ? sessions[0].session_id : ""}
+              />
+              {captivePortalLogoutForm.additional_fields.length > 0
+                ? captivePortalLogoutForm.additional_fields.map(field => (
+                    <input
+                      readOnly
+                      type="text"
+                      name={field.name}
+                      value={field.value}
+                    />
+                  ))
+                : null}
+            </form>
+            <iframe
+              ref={this.logoutIfameRef}
+              name="owisp-auth-logout-iframe"
+              className="owisp-auth-hidden"
+              title="owisp-auth-iframe"
+            />
+          </>
+        )}
       </>
     );
   }
@@ -130,4 +331,22 @@ Status.propTypes = {
   orgSlug: PropTypes.string.isRequired,
   cookies: PropTypes.instanceOf(Cookies).isRequired,
   logout: PropTypes.func.isRequired,
+  captivePortalLoginForm: PropTypes.shape({
+    method: PropTypes.string,
+    action: PropTypes.string,
+    macaddr_param_name: PropTypes.string,
+    fields: PropTypes.shape({
+      username: PropTypes.string,
+      password: PropTypes.string,
+    }),
+    additional_fields: PropTypes.array,
+  }).isRequired,
+  captivePortalLogoutForm: PropTypes.shape({
+    method: PropTypes.string,
+    action: PropTypes.string,
+    fields: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+    additional_fields: PropTypes.array,
+  }).isRequired,
 };
--- server/routes/account.js
+++ server/routes/account.js
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import { Router } from "express";
 
 import obtainToken from "../controllers/obtain-token-controller";
 import passwordChange from "../controllers/password-change-controller";
@@ -6,8 +6,9 @@ import passwordResetConfirm from "../controllers/password-reset-confirm-controll
 import passwordReset from "../controllers/password-reset-controller";
 import registration from "../controllers/registration-controller";
 import validateToken from "../controllers/validate-token-controller";
+import getUserRadiusSessions from "../controllers/user-radius-sessions-controller";
 
-const router = Router({mergeParams: true});
+const router = Router({ mergeParams: true });
 
 router.post("/token", obtainToken);
 router.post("/token/validate", validateToken);
@@ -15,5 +16,6 @@ router.post("/password/change", passwordChange);
 router.post("/password/reset/confirm/", passwordResetConfirm);
 router.post("/password/reset", passwordReset);
 router.post("/", registration);
+router.post("/session/", getUserRadiusSessions);
 
 export default router;
